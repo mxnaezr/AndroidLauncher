e316674b2a00615c66f962824ebf6e0a41d3bd1b
Mona <monawazeer04@gmail.com>
2026-09-19 08:36:27 +0530

    Add app/src/main/java/com/example/launcher/database/AppEntity.kt

---
+++ app/src/main/java/com/example/launcher/database/AppEntity.kt
@@ -0,0 +1,11 @@
+package com.example.launcher.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "apps")
+data class AppEntity(
+    @PrimaryKey val packageName: String,
+    val label: String,
+    val gridPosition: Int = 0
+)
