d943a60087458fbe474d79be5dd6c5233d5ae12d
Mona <monawazeer04@gmail.com>
2026-09-19 08:36:25 +0530

    Add app/src/main/java/com/example/launcher/models/AppInfo.kt

---
+++ app/src/main/java/com/example/launcher/models/AppInfo.kt
@@ -0,0 +1,9 @@
+package com.example.launcher.models
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val packageName: String,
+    val label: String,
+    val icon: Drawable
+)
